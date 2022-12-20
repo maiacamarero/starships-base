b23eb37f5ac4b4d6692dcbc60d95aba2284631e8
maiacamarero <maia.camarero@ing.austral.edu.ar>
2022-12-20 16:45:16 -0300

    inmutabilidad 2.1

--- app/src/main/java/starships/State.java
+++ app/src/main/java/starships/State.java
@@ -2,19 +2,40 @@ package starships;
 
 import starships.collidable.Collidable;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class State {
-    private List<Collidable> elements;
+    private Map<String, Collidable> elementsMap;
     private List<Player> players;
 
     public State(List<Collidable> elements, List<Player> players) {
-        this.elements = elements;
+        this.players = players;
+        this.elementsMap = rancidMethodBecauseJavaSucks(elements);
+    }
+
+    public State(Map<String, Collidable> elementsMap, List<Player> players) {
+        this.elementsMap = elementsMap;
         this.players = players;
     }
 
+    private Map<String, Collidable> rancidMethodBecauseJavaSucks(List<Collidable> elements) {
+        Map<String, Collidable> elementsMap = new HashMap<>();
+        for (Collidable element : elements) {
+            elementsMap.put(element.getId(), element);
+        }
+        return elementsMap;
+    }
+
+    public State setCollidable(Collidable collidable){
+        Map<String, Collidable> newElementsMap = rancidMethodBecauseJavaSucks(elementsMap.values().stream().toList());
+        newElementsMap.put(collidable.getId(), collidable);
+        return new State(elementsMap, players);
+    }
+
     public List<Collidable> getElements() {
-        return elements;
+        return elementsMap.values().stream().toList();
     }
 
     public List<Player> getPlayers() {
